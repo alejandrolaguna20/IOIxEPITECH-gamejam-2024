e669033edbecab9a33d0db28d418bfadd00cb178
Pol Cortadellas Palomino <pol.cortadellas-palomino@epitech.eu>
2024-11-30 21:47:32 +0100

    ADD; try to add credits

---
+++ scripts/credits.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,9 +2,16 @@ extends Control
 
 
 
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	await ready
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
